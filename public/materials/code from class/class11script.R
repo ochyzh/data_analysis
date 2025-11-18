a7197a037529513b82c3f8e0db30eb62df044300
Olga Chyzh <ochyzh1@gmail.com>
2025-11-18 16:30:10 -0500

    maps

--- public/materials/code from class/class11script.R
+++ public/materials/code from class/class11script.R
@@ -2,9 +2,16 @@ library(tidyverse)
 library(mapproj)
 library(maps)
 library(mapdata)
+library(devtools)
+install_github("mccormackandrew/mapcan", build_vignettes = TRUE)
+library(mapcan)
+library(ggplot2)
+
+
+
 states <- map_data("state")
 head(states)
-library(ggplot2)
+
 ggplot() +  geom_path(data=states, aes(x=long, y=lat, group=group),color="black", size=.5)
 #Set theme options:
 theme_set(theme_grey() + theme(axis.text=element_blank(),
@@ -16,27 +23,33 @@ theme_set(theme_grey() + theme(axis.text=element_blank(),
                                panel.border = element_blank(),
                                panel.background = element_blank(),
                                legend.position="none"))
+
 ggplot() +  geom_path(data=states, aes(x=long, y=lat, group=group),color="black", size=.5)+ coord_map()
 statereg<- read.csv("./data/statereg.csv")
 head(statereg)
 states.class.map <- left_join(states, statereg, by = c("region" = "State"))
 head(states.class.map)
-library(devtools)
-install_github("mccormackandrew/mapcan", build_vignettes = TRUE)
-library(mapcan)
+ggplot() +  geom_polygon(data=states.class.map, aes(x=long, y=lat, group=group, fill = StateGroups), colour = I("black"))+ coord_map()+theme(legend.position="bottom")
+
 canada_map<-mapcan(boundaries = "province", type="standard",territories=TRUE)
 head(canada_map)
 ggplot(canada_map, aes(long, lat, group = group)) +
   geom_polygon(color="black", fill="white")
+
 data("federal_election_results")
 federal_election_results %>% as.data.frame() %>%
   dplyr::filter(election_year=="2015")->electdata
 
-animal<- read.csv("./data/animal.csv")
+canada_ridings<-mapcan(boundaries = "ridings", type="standard",territories=TRUE)
+head(canada_ridings)
+canada_ridings %>% left_join(electdata, by="riding_code") %>%
+  ggplot(aes(long, lat, group = group, fill=factor(party)))+
+  geom_polygon(color="black") +scale_fill_discrete("Party", type="qual") + theme(legend.position="bottom")
 
-library(leaflet)
-leaflet() |> addTiles() |>
-  addCircleMarkers(animal$Longitude,animal$Latitude, radius=1)
+#Your Turn:
+
+
+
 
 
 
